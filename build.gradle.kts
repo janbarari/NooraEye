4759866b99fd6f74ada0e2092ae0a9413570aa0e
janbarari <mehdimjt@gmail.com>
2024-01-22 19:21:05 +0330

    feat: add maven publish configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    `maven-publish`
 }
 
+val artifact: String = "NooraEye"
 group = "io.github.janbarari"
 version = "0.1-beta1"
 
@@ -20,4 +22,16 @@ tasks.test {
 
 kotlin {
     jvmToolchain(11)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = group as String
+            artifactId = artifact
+            version = version
+
+            from(components["java"])
+        }
+    }
 }
