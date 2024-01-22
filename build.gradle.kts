9ad5356ad1ca9b2c1bf02a7f6186de80c321c031
Mehdi <mehdimjt@gmail.com>
2024-01-22 19:25:29 +0330

    Publish version 0.1-beta2 (#2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    `maven-publish`
 }
 
+val artifact: String = "NooraEye"
 group = "io.github.janbarari"
-version = "0.1-beta1"
+version = "0.1-beta2"
 
 repositories {
     mavenCentral()
@@ -20,4 +22,16 @@ tasks.test {
 
 kotlin {
     jvmToolchain(11)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = group as String
+            artifactId = artifact
+            version = version
+
+            from(components["java"])
+        }
+    }
 }
